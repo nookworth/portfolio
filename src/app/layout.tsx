d9628debbc4d11fed665ae72e9853901d24f25fc
nookworth <nooksack@gmail.com>
2025-11-05 11:49:44 -0700

    default to light theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <ViewTransitions>
       <html suppressHydrationWarning lang="en">
         <body className={montserrat.className}>
-          <ThemeProvider attribute="class" disableTransitionOnChange>
+          <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange>
             <Nav />
             <main className="text-foreground mx-auto max-w-full px-5 pt-28 pb-10">
               {children}
